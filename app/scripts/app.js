cf9ff143775ba4c68318a2ec6ac861847d26f21d
Ronald Chacon <ronald.chacon87@gmail.com>
2015-12-11 22:34:51 -0800

    Generate angular with yeoman; add post controller and service. all user to create a link. setup firebase

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angNewsApp', ['ngAnimate', 'ngAria', 'ngCookies', 'ngMessages', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'firebase']);
+
+app.constant('FIREBASE_URL', 'https://blistering-torch-477.firebaseio.com/');
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    });
+});
